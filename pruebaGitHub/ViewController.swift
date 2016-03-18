0713eba1627fa8dec6a26477038e1eb25af5d266
SebasFiol <sebasfiol@macbook-pro-de-sebastia.local>
2016-03-18 20:22:48 +0100

    Initial Commit

---
+++ pruebaGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGitHub
+//
+//  Created by SebasFiol on 18/3/16.
+//  Copyright © 2016 SebasFiol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
